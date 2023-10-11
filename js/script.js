b63b9bd225df8b6b32989aa2af5f414e096635d3
dziubenzo <dziubenzo@gmail.com>
2023-10-11 13:52:43 +0200

    Get button textContent when clicked

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@ let firstNumber;
 let secondNumber;
 let operator;
 
+let displayResult = document.querySelector('.display .result');
+let displayOperation = document.querySelector('.display .operation');
+const buttons = document.querySelector('.buttons');
+
+buttons.addEventListener('click', getButton);
+
 // Add two numbers
 function add(num1, num2) {
   return num1 + num2;
@@ -36,4 +42,13 @@ function operate(num1, operator, num2) {
     default:
       return 'Error: improper operator.';
   }
+}
+
+// Get button clicked
+function getButton(event) {
+  // Skip buttons div itself
+  if (event.target === buttons) {
+    return;
+  }
+  return console.log(`${event.target.textContent}`)
 }
